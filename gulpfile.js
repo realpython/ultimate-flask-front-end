33e9af711a69f67fdca527d0792a2c9415da1b5c
Michael Herman <hermanmu@gmail.com>
2015-03-05 09:05:30 -0700

    third tag

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+'use strict';
+
+// requirements
+
+var gulp = require('gulp'),
+    browserify = require('gulp-browserify'),
+    size = require('gulp-size'),
+    clean = require('gulp-clean');
+
+
+// tasks
+
+gulp.task('transform', function () {
+  return gulp.src('./project/static/scripts/jsx/main.js')
+    .pipe(browserify({transform: ['reactify']}))
+    .pipe(gulp.dest('./project/static/scripts/js'))
+    .pipe(size());
+});
+
+gulp.task('clean', function () {
+  return gulp.src(['./project/static/scripts/js'], {read: false})
+    .pipe(clean());
+});
+
+gulp.task('default', ['clean'], function () {
+  gulp.start('transform');
+  gulp.watch('./project/static/scripts/jsx/main.js', ['transform']);
+});
